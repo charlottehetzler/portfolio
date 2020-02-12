900b3ab68126477a861a330334ed8f565a49d6cc
Charlotte Hetzler <charlotte@hetzler.de>
2020-02-12 21:42:34 +0100

    add new html, css, js + links not working

--- application.js
+++ application.js
@@ -1,37 +1,77 @@
-// Scrolling
+$(function(){
+  "use strict";
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-        document.querySelector(this.getAttribute('href')).scrollTo(0, -100);
+  var sect = $( window.location.hash ),
+    portfolio = $('.portfolio-items');
+
+  if(sect.length == 1){
+    $('.section.active').removeClass('active');
+    sect.addClass('active');
+    if( sect.hasClass('border-d') ){
+      $('body').addClass('border-dark');
+    }
+  }
+
+  /*=========================================================================
+    Magnific Popup (Project Popup initialization)
+  =========================================================================*/
+  $('.view-project').magnificPopup({
+    type: 'inline',
+    fixedContentPos: false,
+    fixedBgPos: true,
+    overflowY: 'auto',
+    closeBtnInside: true,
+    preloader: false,
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in'
+  });
+
+  $(window).on('load', function(){
+    $('body').addClass('loaded');
+
+    /*=========================================================================
+      Portfolio Grid
+    =========================================================================*/
+    portfolio.shuffle();
+    $('.portfolio-filters > li > a').on('click', function (e) {
+      e.preventDefault();
+      var groupName = $(this).attr('data-group');
+      $('.portfolio-filters > li > a').removeClass('active');
+      $(this).addClass('active');
+      portfolio.shuffle('shuffle', groupName );
     });
-});
 
+  });
 
-// Color while scrolling
-
-const mainNavLinks = document.querySelectorAll('a[href^="#"]');
-const mainSections = document.querySelectorAll("section");
-
-let lastId;
-let cur = [];
-
-window.addEventListener("scroll", event => {
-  let fromTop = window.scrollY;
-
-  mainNavLinks.forEach(link => {
-    let section = document.querySelector(link.hash);
-    if (
-      section.offsetTop <= fromTop &&
-      section.offsetTop + section.offsetHeight > fromTop
-    ) {
-      link.classList.add("current");
-    } else {
-      link.classList.remove("current");
+  /*=========================================================================
+    Navigation Functions
+  =========================================================================*/
+  $('.section-toggle').on('click', function(){
+    var $this = $(this),
+      sect = $( '#' + $this.data('section') ),
+      current_sect = $('.section.active');
+    if(sect.length == 1){
+      if( sect.hasClass('active') == false && $('body').hasClass('section-switching') == false ){
+        $('body').addClass('section-switching');
+        if( sect.index() < current_sect.index() ){
+          $('body').addClass('up');
+        }else{
+          $('body').addClass('down');
+        }
+        setTimeout(function(){
+          $('body').removeClass('section-switching up down');
+        }, 2500);
+        setTimeout(function(){
+          current_sect.removeClass('active');
+          sect.addClass('active');
+        }, 1250);
+        if( sect.hasClass('border-d') ){
+          $('body').addClass('border-dark');
+        }else{
+          $('body').removeClass('border-dark');
+        }
+      }
     }
   });
 });
